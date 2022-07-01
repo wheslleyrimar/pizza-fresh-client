a836ef7282a23a3041ac05293fcc5659f2bfece2
Wheslley Rimar <wheslley_rimar@hotmail.com>
2022-07-01 17:42:57 -0300

    Navegação nos componentes Settings e NavColumn

--- src/components/NavColumn/index.tsx
+++ src/components/NavColumn/index.tsx
@@ -5,6 +5,7 @@ import * as S from "./style";
 import { RoutePath } from "types/routes";
 import NavColumnItem from "components/NavColumnItem";
 import { HTMLAttributes } from "react";
+import { useNavigate } from "react-router-dom";
 
 type NavColumnType = HTMLAttributes<HTMLDivElement>;
 
@@ -13,6 +14,7 @@ type NavColumnProps = {
 } & NavColumnType;
 
 const NavColumn = ({activeRoute}: NavColumnProps) => {
+    const navigate = useNavigate();
     const items = [
         {
             icon: <Market />,
@@ -37,6 +39,7 @@ const NavColumn = ({activeRoute}: NavColumnProps) => {
         <S.NavColumn>
             {items.map((i, key) => (
             <NavColumnItem 
+                onClick={() => navigate(i.navigation)}
                 active={i.navigation === activeRoute}
                 icon={i.icon}
                 title={i.title}
--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -3,13 +3,17 @@ import Menu from "components/Menu";
 import { RoutePath } from "types/routes";
 import {navigationItems} from "data/navigation";
 import * as S from "./style";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import NavColumn from "components/NavColumn";
 
 
 const Settings = () => {
     const navigate = useNavigate();
     const handleNavigation = (path: RoutePath) => navigate(path);
+    const { pathname } = useLocation();
+
+    const splitterPath = (path: string) => path.split("/").pop() as RoutePath;
+    const path = splitterPath(pathname);
     
     return (
         <S.Settings>
@@ -25,12 +29,14 @@ const Settings = () => {
                 </header>
                 <S.SettingsContent>
                     <S.SettingsContentSidebar>
-                        {/* Trecho precisa ser alterado posteriormente */}
-                        <NavColumn activeRoute={RoutePath.SETTINGS_TABLES}/>
+                        <NavColumn activeRoute={path}/>
                     </S.SettingsContentSidebar>
                     <S.SettingsContentBox>
-                        <S.SettingsContentBoxEmpty>Selecione uma categoria</S.SettingsContentBoxEmpty>
-                        <Outlet />
+                        {path === splitterPath(RoutePath.SETTINGS) ? (
+                            <S.SettingsContentBoxEmpty>Selecione uma categoria</S.SettingsContentBoxEmpty>
+                            ) : (
+                                <Outlet />
+                        )}
                     </S.SettingsContentBox>
                 </S.SettingsContent>
             </S.SettingsPage>

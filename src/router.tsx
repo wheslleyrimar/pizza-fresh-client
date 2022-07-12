a94471d80410d36b76ebb87cd6bc28f420b7006a
Wheslley Rimar <wheslley_rimar@hotmail.com>
2022-07-12 11:18:10 -0300

    Rotas Autenticadas (AuthenticatedRoutes)

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import { RoutePath } from 'types/routes';
 import Home from "pages/Home/index";
 import Login from 'pages/Login';
@@ -6,15 +6,24 @@ import ManageProducts from 'components/ManageProducts';
 import ManageUsers from 'components/ManageUsers';
 import Settings from 'pages/Settings';
 import ManageTables from 'components/ManageTables';
+import { Auth } from 'helpers/Auth';
+
+
+const AuthenticatedRoutes = () => {
+    const isAuthenticated = Auth.isAuth();
+    return isAuthenticated ? <Outlet /> : <Navigate to={RoutePath.LOGIN} />
+}
 const Router = () => {
     return (
         <Routes>
             <Route path={RoutePath.LOGIN} element={<Login />} />
-            <Route path={RoutePath.HOME} element={<Home />} />
-            <Route path={RoutePath.SETTINGS} element={<Settings />}>
-                <Route path={RoutePath.SETTINGS_TABLES} element={<ManageTables />} />
-                <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
-                <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+            <Route path="/" element={<AuthenticatedRoutes />}>
+                <Route path={RoutePath.HOME} element={<Home />} />
+                <Route path={RoutePath.SETTINGS} element={<Settings />}>
+                    <Route path={RoutePath.SETTINGS_TABLES} element={<ManageTables />} />
+                    <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
+                    <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+                </Route>
             </Route>
         </Routes>
     );

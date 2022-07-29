340b2bd942c19272842e3291f48e06d0f5c18478
Wheslley Rimar <wheslley_rimar@hotmail.com>
2022-07-28 22:05:03 -0300

    Gerenciamento de Produtos - Adição, Edição e Exclusão

--- src/components/EditProduct/index.tsx
+++ src/components/EditProduct/index.tsx
@@ -1,41 +1,100 @@
 import { ReactComponent as Pencil } from "assets/icons/edit.svg";
+import { useEffect, useState } from "react";
+import { ProductResponse } from "types/api/product";
 import * as S from "./style";
 
+interface EditProductProps {
+    product: ProductResponse;
+    onEdit: (data: ProductResponse) => void;
+    onDelete: (data: ProductResponse) => void;
+    onCancel: boolean;
+}
 
-const EditProduct = () => {
+const EditProduct = ({product, onEdit, onDelete, onCancel}: EditProductProps) => {
+
+    const [isEditing, setIsEditing] = useState(false);
+    
+    const form = {
+        name: product.name,
+        price: product.price,
+        image: product.image,
+        description: product.description,
+    };
+
+    const [state, setState] = useState(form);
+
+      const productEditFormatter = (toFormat: typeof form): ProductResponse => ({
+        id: product.id,
+        name: toFormat.name,
+        price: toFormat.price,
+        description: toFormat.description,
+        image: toFormat.image,
+      });
+
+      const handleChange = (name: string, value: string) => {
+        setState({ ...state, [name]: value });
+        const productFormatted = productEditFormatter(state);
+        onEdit(productFormatted);
+      };
+    
+      const onEditClick = () => {
+        setIsEditing(true);
+        const productFormatted = productEditFormatter(state);
+        onEdit(productFormatted);
+      };
+    
+      useEffect(() => {
+        setIsEditing(false);
+      }, [onCancel]);
+
     return (
-        <S.EditProduct>
+        <S.EditProduct role="listitem">
+            {!isEditing ? (
             <>
-                <S.EditProductImage src="" alt="Pizza de..." />
+                <S.EditProductImage src={product.image} alt={`Pizza de ${product.name}`} />
                 <S.EditProductDetails>
-                    <S.EditProductDetailsName>Nome do Produto</S.EditProductDetailsName>
-                    <S.EditProductDetailsPrice>R$ 00,00</S.EditProductDetailsPrice>
-                    <S.EditProductDetailsDescription>Descrição do Produto</S.EditProductDetailsDescription>
+                    <S.EditProductDetailsName>{product.name}</S.EditProductDetailsName>
+                    <S.EditProductDetailsPrice>{product.price}</S.EditProductDetailsPrice>
+                    <S.EditProductDetailsDescription>{product.description}</S.EditProductDetailsDescription>
                 </S.EditProductDetails>
 
-                <S.EditProductAction>
+                <S.EditProductAction onClick={() => onEditClick()}>
                     <Pencil /> Editar
                 </S.EditProductAction>
             </>
+            ) : (
             <S.EditFormGroup>
                 <S.EditForm
                     type="text"
                     placeholder="Título"
+                    success={Boolean(state.name.length)}
+                    value={state.name}
+                    onChange={({ target }) => handleChange("name", target.value)}
                 />
                 <S.EditForm
                     type="number"
                     placeholder="Preço"
+                    success={Boolean(state.price.toString().length)}
+                    value={state.price || ""}
+                    onChange={({ target }) => handleChange("price", target.value)}
                 />
                 <S.EditForm
                     type="text"
                     placeholder="Descrição"
+                    success={Boolean(state.description.length)}
+                    value={state.description}
+                    onChange={({ target }) => handleChange("description", target.value)}
                 />
                 <S.EditForm
                     type="url"
                     placeholder="Imagem"
+                    success={Boolean(state.image.length)}
+                    value={state.image}
+                    onChange={({ target }) => handleChange("image", target.value)}
                 />
-                <S.Delete>Deletar</S.Delete>
+                <S.Delete onClick={()=> onDelete(product)}>Deletar</S.Delete>
             </S.EditFormGroup>
+        )}
         </S.EditProduct>
     );
 }
--- src/components/ManageProducts/index.tsx
+++ src/components/ManageProducts/index.tsx
@@ -1,6 +1,12 @@
 import { ReactComponent as Add } from "assets/icons/add.svg";
 import EditProduct from "components/EditProduct";
-import { HTMLAttributes } from "react";
+import { user } from "helpers/endpoints/user";
+import { HTMLAttributes, useState, useEffect } from "react";
+import { useMutation, useQuery } from "react-query";
+import { ProductService } from "services/ProductService";
+import { ErrorResponse } from "types/api/error";
+import { Product, ProductResponse } from "types/api/product";
+import { QueryKey } from "types/QueryKey";
 import * as S from "./style";
 
 type ManageProductsType = HTMLAttributes<HTMLDivElement>;
@@ -8,7 +14,132 @@ type ManageProductsType = HTMLAttributes<HTMLDivElement>;
 type ManageProductsProps = {} & ManageProductsType;
 
 
-const ManageProducts = ({...props}: ManageProductsProps) => {
+const ManageProducts = ({ ...props }: ManageProductsProps) => {
+
+    const [products, setProducts] = useState<ProductResponse[]>([]);
+    const { data: productsData } = useQuery(
+        QueryKey.PRODUCTS,
+        ProductService.getLista
+    );
+
+    const add = useMutation(ProductService.create, {
+        onSuccess: (data: ProductResponse & ErrorResponse) => {
+            if (data.statusCode) {
+                return;
+            }
+
+            const productList = [...products, data as ProductResponse];
+            setProducts(productList);
+        },
+        onError: () => {
+            console.error("Erro ao adicionar um produto");
+        },
+    });
+
+    const update = useMutation(ProductService.updateById, {
+        onSuccess: (data: ProductResponse & ErrorResponse) => {
+            if (data.statusCode) {
+                return;
+            }
+
+            const editedUsers = products.map((i) =>
+                data.id === i.id ? (data as ProductResponse) : i
+            );
+            setProducts(editedUsers);
+        },
+        onError: () => {
+            console.error("Erro ao atualizar o produto");
+        },
+    });
+
+
+    const remove = useMutation(ProductService.deleteById, {
+        onSuccess: (data: ProductResponse & ErrorResponse) => {
+            if (data.statusCode) {
+                return;
+            }
+
+            const editedProducts = products.filter((i) => data.id !== i.id);
+            setProducts(editedProducts);
+        },
+        onError: () => {
+            console.error("Erro ao remover o protudo");
+        },
+    });
+
+    let productsToEdit: ProductResponse[] = [];
+
+    const onEditProduct = (toEdit: ProductResponse) => {
+        setCancel(false);
+        const existing = productsToEdit.find((user) => user.id === toEdit.id);
+
+        productsToEdit = existing
+            ? productsToEdit.map((i) => (i.id === existing.id ? toEdit : i))
+            : [...productsToEdit, toEdit];
+    }
+
+    const form = {
+        name: "",
+        price: Number(""),
+        image: "",
+        description: "",
+    };
+
+    const [isAdding, setIsAdding] = useState(false);
+    const [productToAdd, setProductToAdd] = useState(form);
+
+    const handleAddChange = (name: string, value: string | number) => {
+        setProductToAdd({ ...productToAdd, [name]: value });
+    };
+
+
+    const productIsValid = () =>
+        Boolean(
+            productToAdd.name.length &&
+            productToAdd.price.toString().length &&
+            productToAdd.description.length &&
+            productToAdd.image.length
+        );
+
+    const productFormatter = (toFormat: typeof form): Product => ({
+        name: toFormat.name,
+        price: toFormat.price,
+        description: toFormat.description,
+        image: toFormat.image,
+    });
+
+    const [cancel, setCancel] = useState(false);
+
+    const handleCancel = () => {
+        setCancel(true);
+        setIsAdding(false);
+        setTimeout(() => setCancel(false));
+        productsToEdit = [];
+    }
+
+    const handleSave = () => {
+        const canAdd = productIsValid();
+        const productFormatted = productFormatter(productToAdd);
+
+        productsToEdit.forEach((product) =>
+            update.mutate({ product, id: product.id })
+        );
+
+        if (canAdd) add.mutate(productFormatted);
+        setTimeout(() => handleCancel(), 300);
+        setProductToAdd(form);
+        setIsAdding(false);
+    };
+
+    const handleDelete = (productToDelete: ProductResponse) => {
+        remove.mutate(productToDelete.id);
+        handleCancel();
+    }
+
+    useEffect(() => {
+        setProducts(productsData || []);
+    }, [productsData]);
+
     return (
         <S.ManageProducts {...props}>
             <S.ManageProductsTitle>Gerenciar Produtos</S.ManageProductsTitle>
@@ -16,33 +147,56 @@ const ManageProducts = ({...props}: ManageProductsProps) => {
                 <b>Pizzas</b>
             </S.ManageProductsSub>
             <S.ManageProductsContent>
-                <S.ManageProductsContentAdd>
-                    <Add />
-                    <span>Adicionar Pizza</span>
-                </S.ManageProductsContentAdd>
-                <S.AddCard>
-                    <S.EditForm
-                        type="text"
-                        placeholder="Título"    
+                {!isAdding ? (
+                    <S.ManageProductsContentAdd onClick={() => setIsAdding(!isAdding)}>
+                        <Add />
+                        <span>Adicionar Pizza</span>
+                    </S.ManageProductsContentAdd>
+                ) : (
+                    <S.AddCard>
+                        <S.EditForm
+                            type="text"
+                            placeholder="Título"
+                            success={Boolean(productToAdd.name.length)}
+                            value={productToAdd.name}
+                            onChange={({ target }) => handleAddChange("name", target.value)}
+                        />
+                        <S.EditForm
+                            type="number"
+                            placeholder="Preço"
+                            success={Boolean(productToAdd.price)}
+                            value={productToAdd.price || ""}
+                            onChange={({ target }) => handleAddChange("price", +target.value)}
+                        />
+                        <S.EditForm
+                            type="text"
+                            placeholder="Descrição"
+                            success={Boolean(productToAdd.description.length)}
+                            value={productToAdd.description}
+                            onChange={({ target }) => handleAddChange("description", target.value)}
+                        />
+                        <S.EditForm
+                            type="url"
+                            placeholder="Imagem"
+                            success={Boolean(productToAdd.image.length)}
+                            value={productToAdd.image}
+                            onChange={({ target }) => handleAddChange("image", target.value)}
+                        />
+                    </S.AddCard>
+                )}
+                {products.map((product, index) => (
+                    <EditProduct
+                        product={product}
+                        key={index}
+                        onEdit={onEditProduct}
+                        onCancel={cancel}
+                        onDelete={handleDelete}
                     />
-                    <S.EditForm 
-                        type="number"
-                        placeholder="Preço"
-                    />
-                    <S.EditForm
-                        type="text"
-                        placeholder="Descrição"
-                    />
-                    <S.EditForm
-                        type="url"
-                        placeholder="Imagem"
-                    />
-                </S.AddCard>
-                <EditProduct />
+                ))}
             </S.ManageProductsContent>
             <S.ManageProductsActions>
-                <S.ManageProductsActionsCancel>Cancelar</S.ManageProductsActionsCancel>
-                <S.ManageProductsActionsSave>Salvar Mudanças</S.ManageProductsActionsSave>
+                <S.ManageProductsActionsCancel onClick={handleCancel}>Cancelar</S.ManageProductsActionsCancel>
+                <S.ManageProductsActionsSave onClick={handleSave}>Salvar Mudanças</S.ManageProductsActionsSave>
             </S.ManageProductsActions>
         </S.ManageProducts>
     );

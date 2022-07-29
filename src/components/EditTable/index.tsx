6d316d51145b9e08528c08b924ca0f8916765e53
Wheslley Rimar <wheslley_rimar@hotmail.com>
2022-07-29 15:40:16 -0300

    Funcionalidade de Gerenciamento de Mesas (ManageTables - Adição, Edição e Exclusão)

--- src/components/EditTable/index.tsx
+++ src/components/EditTable/index.tsx
@@ -1,29 +1,57 @@
 import { ReactComponent as Pencil } from "assets/icons/edit.svg";
+import { useEffect, useState } from "react";
+import { TableResponse } from "types/api/table";
 import * as S from "./style";
 
+interface EditTableProps {
+    table: TableResponse;
+    onCancel: boolean;
+    onDelete: (data: TableResponse) => void;
+    onEdit: (data: TableResponse) => void;
+}
+const EditTable = ({ table, onCancel, onDelete, onEdit }: EditTableProps) => {
 
-const EditTable = () => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    useEffect(() => {
+        setIsEditing(false);
+    }, [onCancel]);
+
+    const onEditClick = () => {
+        setIsEditing(true);
+        onEdit(table);
+    };
+
+    const onEditChange = (tableId: string) => {
+        setIsEditing(true);
+        onEdit({ ...table, number: Number(tableId) });
+    };
+
 
     return (
-        <S.EditTable>
-            <>
-                <S.EditTableDetails>
-                    <S.EditTableDetailsName>Número da mesa</S.EditTableDetailsName>
-                </S.EditTableDetails>
-                <S.EditTableAction>
-                    <Pencil /> Editar
-                </S.EditTableAction>
-            </>
+        <S.EditTable role="listitem">
+            {!isEditing ? (
+                <>
+                    <S.EditTableDetails>
+                        <S.EditTableDetailsName>{table.number}</S.EditTableDetailsName>
+                    </S.EditTableDetails>
+                    <S.EditTableAction onClick={() => onEditClick()}>
+                        <Pencil /> Editar
+                    </S.EditTableAction>
+                </>
+            ) : (
 
-            <>
-                <label htmlFor="tableId">Número da mesa</label>
-                <S.EditForm 
-                    id="tableId"
-                    type="number"
-                    placeholder="01"
-                />
-                <S.Deletar>Deletar Mesa</S.Deletar>
-            </>
+                <>
+                    <label htmlFor="tableId">Número da mesa</label>
+                    <S.EditForm
+                        id="tableId"
+                        type="number"
+                        placeholder="01"
+                        onChange={({ target }) => onEditChange(target.value)}
+                    />
+                    <S.Deletar onClick={() => onDelete(table)}>Deletar Mesa</S.Deletar>
+                </>
+            )}
         </S.EditTable>
     );
 };
--- src/components/ManageTables/index.tsx
+++ src/components/ManageTables/index.tsx
@@ -1,40 +1,147 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useEffect, useState } from "react";
 import { ReactComponent as Add } from "assets/icons/add.svg";
 import * as S from "./style";
 import EditTable from "components/EditTable";
+import { useMutation, useQuery } from "react-query";
+import { QueryKey } from "types/QueryKey";
+import { TableService } from "services/TableService";
+import { Table, TableResponse } from "types/api/table";
+import { ErrorResponse } from "types/api/error";
 
 type ManageTablesType = HTMLAttributes<HTMLDivElement>;
 
 type ManageTablesProps = {} & ManageTablesType;
 
-const ManageTables = ({...props}:ManageTablesProps) => {
+const ManageTables = ({ ...props }: ManageTablesProps) => {
+  const [tables, setTables] = useState<TableResponse[]>([]);
 
+  const add = useMutation(TableService.create, {
+    onSuccess: (data: TableResponse & ErrorResponse) => {
+      if (data.statusCode) {
+        return;
+      }
+
+      const tableList = [...tables, data as TableResponse];
+      setTables(tableList);
+    },
+    onError: () => {
+      console.error("Erro ao adicionar a mesa");
+    },
+  });
+
+  const update = useMutation(TableService.updateById, {
+    onSuccess: (data: TableResponse & ErrorResponse) => {
+      if (data.statusCode) {
+        return;
+      }
+  
+      const editedTables = tables.map((i) =>
+        data.id === i.id ? (data as TableResponse) : i
+      );
+      setTables(editedTables);
+    },
+    onError: () => {
+      console.error("Erro ao atualizar a mesa");
+    },
+  });
+
+  const remove = useMutation(TableService.deleteById, {
+    onSuccess: (data: TableResponse & ErrorResponse) => {
+      if (data.statusCode) {
+        return;
+      }
+  
+      const editedTables = tables.filter((i) => data.id !== i.id);
+      setTables(editedTables);
+    },
+    onError: () => {
+      console.error("Erro ao remover a mesa");
+    },
+  });
+
+  const [cancel, setCancel] = useState(false);
+  const [isAdding, setIsAdding] = useState(false);
+  
+  let tableToAdd: Table | null = null;
+  let tablesToEdit: TableResponse[] = [];
+
+
+  const handleCancel = () => {
+    setCancel(true);
+    setIsAdding(false);
+    setTimeout(() => setCancel(false));
+    tablesToEdit = [];
+  };
+
+  const handleSave = () => {
+    tablesToEdit.forEach((table) => update.mutate(table));
+    if (tableToAdd) add.mutate(tableToAdd);
+    setTimeout(() => handleCancel(), 300);
+    tableToAdd = null;
+    setIsAdding(false);
+  };
+
+  const handleDelete = (table: TableResponse) => {
+    remove.mutate(table.id);
+  };
+
+  const onAddChange = (number: string) => {
+    tableToAdd = { number: Number(number) };
+  };
+
+  const onEditTable = (toEdit: TableResponse) => {
+    setCancel(false);
+    const existing = tablesToEdit.find((table) => table.id === toEdit.id);
+  
+    tablesToEdit = existing
+      ? tablesToEdit.map((i) => (i.id === existing.id ? toEdit : i))
+      : [...tablesToEdit, toEdit];
+  };
+
+  const { data: tablesData } = useQuery(QueryKey.TABLES, TableService.getLista);
+
+  useEffect(() => {
+    setTables(tablesData || []);
+  }, [tablesData]);
   return (
-      <S.ManageTables  {...props}>
-          <S.ManageTablesTitle>Gerenciar Mesas</S.ManageTablesTitle>
-          <S.ManageTablesSub>
-              <b>Mesas</b>
-          </S.ManageTablesSub>
-          <S.ManageTablesContent>
-              <S.ManageTablesContentAdd>
-                <Add />
-                <span>Adicionar mesa</span>
-              </S.ManageTablesContentAdd>
-              <S.ManageTablesContentAdd>
-                  <label htmlFor="tableId">Número da Mesa</label>
-                  <S.EditForm
-                    id="tableId"
-                    type="number"
-                    placeholder="01"
-                  />
-              </S.ManageTablesContentAdd>
-              <EditTable />
-          </S.ManageTablesContent>
-          <S.ManageTablesActions>
-                <S.ManageTablesActionsCancel>Cancelar</S.ManageTablesActionsCancel>
-                <S.ManageTablesActionsSave>Salvar Mudanças</S.ManageTablesActionsSave>
-          </S.ManageTablesActions>
-      </S.ManageTables>
+    <S.ManageTables  {...props}>
+      <S.ManageTablesTitle>Gerenciar Mesas</S.ManageTablesTitle>
+      <S.ManageTablesSub>
+        <b>Mesas</b>
+      </S.ManageTablesSub>
+      <S.ManageTablesContent>
+        {!isAdding ? (
+        <S.ManageTablesContentAdd onClick={()=> setIsAdding(true)}>
+          <Add />
+          <span>Adicionar mesa</span>
+        </S.ManageTablesContentAdd>
+        ) : (
+        <S.ManageTablesContentAdd>
+          <label htmlFor="tableId">Número da Mesa</label>
+          <S.EditForm
+            id="tableId"
+            type="number"
+            placeholder="01"
+            onChange={({target}) => onAddChange(target.value)}
+          />
+        </S.ManageTablesContentAdd>
+        )
+      }
+        {tables.map((table, index) => (
+          <EditTable
+            table={table}
+            key={index}
+            onCancel={cancel}
+            onDelete={handleDelete}
+            onEdit={onEditTable} />
+        ))}
+
+      </S.ManageTablesContent>
+      <S.ManageTablesActions>
+        <S.ManageTablesActionsCancel onClick={handleCancel}>Cancelar</S.ManageTablesActionsCancel>
+        <S.ManageTablesActionsSave onClick={handleSave}>Salvar Mudanças</S.ManageTablesActionsSave>
+      </S.ManageTablesActions>
+    </S.ManageTables>
   );
 };
 

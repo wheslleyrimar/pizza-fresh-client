10d1aef72501dd6a949ece16aa3fe7f0d6e42f70
Wheslley Rimar <wheslley_rimar@hotmail.com>
2022-07-29 21:01:34 -0300

    Funcionalidade de Gerenciamento de Usuários (ManageUsers - Adição, Edição e Exclusão)

--- src/components/EditUser/index.tsx
+++ src/components/EditUser/index.tsx
@@ -1,49 +1,109 @@
 import { ReactComponent as Pencil } from "assets/icons/edit.svg";
+import { useEffect, useState } from "react";
+import { User, UserResponse, UserUpdate } from "types/api/user";
 import * as S from "./style";
 
+interface EditUserProps {
+    user: UserResponse;
+    onCancel: boolean;
+    onDelete: (data: UserResponse) => void;
+    onEdit: (data: UserUpdate) => void;
+}
 
-const EditUser = () => {
+const EditUser = ({ user, onCancel, onDelete, onEdit }: EditUserProps) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const form = {
+        id: user.id,
+        nick: user.nickname,
+        name: user.name,
+        image: user.image,
+        pass: "",
+        passConfirm: "",
+    };
+
+    const [state, setState] = useState(form);
+
+
+    const userEditFormatter = (toFormat: typeof form): User => ({
+        nickname: toFormat.nick,
+        name: toFormat.name,
+        password: toFormat.pass,
+        passwordConfirm: toFormat.passConfirm,
+        image: toFormat.image,
+    });
+
+    const handleChange = (name: string, value: string) => {
+        setState({ ...state, [name]: value });
+        const userFormatted = { user: userEditFormatter(state), id: user.id };
+        onEdit(userFormatted);
+    };
+
+    const onEditClick = () => {
+        setIsEditing(true);
+        const userFormatted = { user: userEditFormatter(state), id: user.id };
+        onEdit(userFormatted);
+    };
+
+    useEffect(() => {
+        setIsEditing(false);
+    }, [onCancel]);
+
     return (
-        <S.EditUser>
-            <>
-                <S.EditUserDetails>
-                    <S.EditUserDetailsImageWrap>
-                        <S.EditUserDetailsImage src="" alt="Foto de..." />
-                    </S.EditUserDetailsImageWrap>
-                    <S.EditUserDetailsTitle>Nome</S.EditUserDetailsTitle>
-                    <S.EditUserDetailsText>
-                        <b>usuário: </b> {/* usuário */}
-                    </S.EditUserDetailsText>
-                </S.EditUserDetails>
+        <S.EditUser role="listitem">
+            {!isEditing ? (
+                <>
+                    <S.EditUserDetails>
+                        <S.EditUserDetailsImageWrap>
+                            <S.EditUserDetailsImage src={user.image} alt={`Foto de ${user.name}`} />
+                        </S.EditUserDetailsImageWrap>
+                        <S.EditUserDetailsTitle>{user.name}</S.EditUserDetailsTitle>
+                        <S.EditUserDetailsText>
+                            <b>usuário: </b> {user.nickname}
+                        </S.EditUserDetailsText>
+                    </S.EditUserDetails>
 
-                <S.EditUserAction>
-                    <Pencil /> Editar
-                </S.EditUserAction>
+                    <S.EditUserAction  onClick={() => onEditClick()}>
+                        <Pencil /> Editar
+                    </S.EditUserAction>
 
-            </>
-            <S.EditFormGroup>
-                <S.EditForm
-                    type="text"
-                    placeholder="Nome"
-                />
-                <S.EditForm
-                    type="text"
-                    placeholder="Nome de usuário"
-                />
-                <S.EditForm
-                    type="password"
-                    placeholder="Senha"
-                />
-                <S.EditForm
-                    type="password"
-                    placeholder="Confirmar Senha"
-                />
-                <S.EditForm
-                    type="url"
-                    placeholder="Imagem"
-                />
-                <S.Delete>Deletar</S.Delete>
-            </S.EditFormGroup>
+                </>
+            ) : (
+                <S.EditFormGroup>
+                    <S.EditForm
+                        type="text"
+                        placeholder="Nome"
+                        value={state.name}
+                        onChange={({ target }) => handleChange("name", target.value)}
+                    />
+                    <S.EditForm
+                        type="text"
+                        placeholder="Nome de usuário"
+                        value={state.nick}
+                        onChange={({ target }) => handleChange("nick", target.value)}
+                    />
+                    <S.EditForm
+                        type="password"
+                        placeholder="Senha"
+                        value={state.pass}
+                        onChange={({ target }) => handleChange("pass", target.value)}
+                    />
+                    <S.EditForm
+                        type="password"
+                        error={Boolean(state.passConfirm.length && state.pass.length && state.pass !== state.passConfirm)}
+                        placeholder="Confirmar senha"
+                        value={state.passConfirm}
+                        onChange={({ target }) => handleChange("passConfirm", target.value)}
+                    />
+                    <S.EditForm
+                        type="url"
+                        value={state.image}
+                        placeholder="Imagem"
+                        onChange={({ target }) => handleChange("image", target.value)}
+                    />
+                    <S.Delete onClick={() => onDelete(user)}>Deletar</S.Delete>
+                </S.EditFormGroup>
+            )}
         </S.EditUser>
     );
 }
--- src/components/ManageUsers/index.tsx
+++ src/components/ManageUsers/index.tsx
@@ -1,6 +1,11 @@
 import { ReactComponent as Add } from "assets/icons/add.svg";
 import EditUser from "components/EditUser";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useEffect, useState } from "react";
+import { useMutation, useQuery } from "react-query";
+import { UserService } from "services/UserService";
+import { ErrorResponse } from "types/api/error";
+import { User, UserResponse, UserUpdate } from "types/api/user";
+import { QueryKey } from "types/QueryKey";
 import * as S from "./style";
 
 type ManageUsersType = HTMLAttributes<HTMLDivElement>;
@@ -9,6 +14,135 @@ type ManageUsersProps = {} & ManageUsersType;
 
 
 const ManageUsers = ({ ...props }: ManageUsersProps) => {
+    const [users, setUsers] = useState<UserResponse[]>([]);
+    const { data: usersData } = useQuery(QueryKey.USERS, UserService.getLista);
+
+    const add = useMutation(UserService.create, {
+        onSuccess: (data: UserResponse & ErrorResponse) => {
+            if (data.statusCode) {
+                return;
+            }
+
+            const userList = [...users, data as UserResponse];
+            setUsers(userList);
+        },
+        onError: () => {
+            console.error("Erro ao criar um usuário");
+        },
+    });
+
+    const remove = useMutation(UserService.deleteById, {
+        onSuccess: (data: UserResponse & ErrorResponse) => {
+            if (data.statusCode) {
+                return;
+            }
+
+            const editedUsers = users.filter((i) => data.id !== i.id);
+            setUsers(editedUsers);
+        },
+        onError: () => {
+            console.error("Erro ao remover o usuário");
+        },
+    });
+
+    const update = useMutation(UserService.updateById, {
+        onSuccess: (data: UserResponse & ErrorResponse) => {
+            if (data.statusCode) {
+                return;
+            }
+
+            const editedUsers = users.map((i) =>
+                data.id === i.id ? (data as UserResponse) : i
+            );
+            setUsers(editedUsers);
+        },
+        onError: () => {
+            console.error("Erro ao atualizar a mesa");
+        },
+    });
+
+
+
+    const [isAdding, setIsAdding] = useState(false);
+    const [cancel, setCancel] = useState(false);
+
+    type usersToEditType = { id: string } & User;
+
+    let usersToEdit: usersToEditType[] = [];
+
+    const form = {
+        nick: "",
+        name: "",
+        image: "",
+        pass: "",
+        passConfirm: "",
+    };
+
+    const [userToAdd, setUserToAdd] = useState(form);
+
+    const handleAddChange = (name: string, value: string) => {
+        setUserToAdd({ ...userToAdd, [name]: value });
+    };
+
+
+    const onEditUser = (toEdit: UserUpdate) => {
+        setCancel(false);
+        const existing = usersToEdit.find((user) => user.id === toEdit.id);
+
+        const userFormatted = { ...toEdit.user, id: toEdit.id };
+
+        usersToEdit = existing
+            ? usersToEdit.map((i) => (i.id === existing.id ? userFormatted : i))
+            : [...usersToEdit, userFormatted];
+    };
+
+    const handleCancel = () => {
+        setCancel(true);
+        setIsAdding(false);
+        setTimeout(() => setCancel(false));
+        usersToEdit = [];
+    };
+
+    const handleDelete = (user: UserResponse) => {
+        remove.mutate(user.id);
+        handleCancel();
+    };
+
+
+    const userIsValid = () =>
+        Boolean(
+            userToAdd.name.length &&
+            userToAdd.nick.length &&
+            userToAdd.pass.length &&
+            userToAdd.passConfirm.length &&
+            userToAdd.image.length &&
+            userToAdd.pass === userToAdd.passConfirm
+        );
+
+    const userFormatter = (toFormat: typeof form): User => ({
+        nickname: toFormat.nick,
+        name: toFormat.name,
+        password: toFormat.pass,
+        passwordConfirm: toFormat.passConfirm,
+        image: toFormat.image,
+    });
+
+    const handleSave = () => {
+        const canAdd = userIsValid();
+        const user = userFormatter(userToAdd);
+
+        usersToEdit.forEach((user) => update.mutate({ user, id: user.id }));
+
+        if (canAdd) add.mutate(user);
+        setTimeout(() => handleCancel(), 300);
+        setUserToAdd(form);
+        setIsAdding(false);
+    };
+
+    useEffect(() => {
+        setUsers(usersData || []);
+    }, [usersData]);
+
     return (
         <S.ManageUsers {...props}>
             <S.ManageUsersTitle>Gerenciar Usuários</S.ManageUsersTitle>
@@ -16,37 +150,66 @@ const ManageUsers = ({ ...props }: ManageUsersProps) => {
                 <b>Usuários</b>
             </S.ManageUsersSub>
             <S.ManageUsersContent>
-                <S.ManageUsersContentAdd>
-                    <Add />
-                    <span>Adicionar Usuário</span>
-                </S.ManageUsersContentAdd>
-                <S.ManageUsersContentAdd>
-                    <S.EditForm
-                        type="text"
-                        placeholder="Nome"
+                {!isAdding ? (
+                    <S.ManageUsersContentAdd onClick={() => setIsAdding(true)}>
+                        <Add />
+                        <span>Adicionar Usuário</span>
+                    </S.ManageUsersContentAdd>
+                ) : (
+                    <S.ManageUsersContentAdd>
+                        <S.EditForm
+                            type="text"
+                            placeholder="Nome"
+                            success={Boolean(userToAdd.name.length)}
+                            value={userToAdd.name}
+                            onChange={({ target }) => handleAddChange("name", target.value)}
+                        />
+                        <S.EditForm
+                            type="text"
+                            placeholder="Nome de usuário"
+                            success={Boolean(userToAdd.nick.length)}
+                            value={userToAdd.nick}
+                            onChange={({ target }) => handleAddChange("nick", target.value)}
+                        />
+                        <S.EditForm
+                            type="password"
+                            placeholder="Senha"
+                            minLength={6}
+                            success={Boolean(userToAdd.pass.length)}
+                            value={userToAdd.pass}
+                            onChange={({ target }) => handleAddChange("pass", target.value)}
+                        />
+                        <S.EditForm
+                            type="password"
+                            minLength={6}
+                            success={Boolean(userToAdd.passConfirm.length && userToAdd.pass === userToAdd.passConfirm)}
+                            error={Boolean(userToAdd.passConfirm.length && userToAdd.pass.length && userToAdd.pass !== userToAdd.passConfirm)}
+                            placeholder="Confirmar senha"
+                            value={userToAdd.passConfirm}
+                            onChange={({ target }) => handleAddChange("passConfirm", target.value)}
+                        />
+                        <S.EditForm
+                            type="url"
+                            placeholder="Imagem"
+                            success={Boolean(userToAdd.image.length)}
+                            value={userToAdd.image}
+                            onChange={({ target }) => handleAddChange("image", target.value)}
+                        />
+                    </S.ManageUsersContentAdd>
+                )}
+                {users.map((user, index) => (
+                    <EditUser
+                        user={user}
+                        key={index}
+                        onEdit={onEditUser}
+                        onCancel={cancel}
+                        onDelete={handleDelete}
                     />
-                    <S.EditForm
-                        type="text"
-                        placeholder="Nome de usuário"
-                    />
-                    <S.EditForm
-                        type="password"
-                        placeholder="Senha"
-                    />
-                    <S.EditForm
-                        type="password"
-                        placeholder="Confirmar Senha"
-                    />
-                    <S.EditForm
-                        type="url"
-                        placeholder="Imagem"
-                    />
-                </S.ManageUsersContentAdd>
-                <EditUser />
+                ))}
             </S.ManageUsersContent>
             <S.ManageUsersActions>
-                <S.ManageUsersActionsCancel>Cancelar</S.ManageUsersActionsCancel>
-                <S.ManageUsersActionsSave>Salvar Mudanças</S.ManageUsersActionsSave>
+                <S.ManageUsersActionsCancel onClick={handleCancel}>Cancelar</S.ManageUsersActionsCancel>
+                <S.ManageUsersActionsSave onClick={handleSave}>Salvar Mudanças</S.ManageUsersActionsSave>
             </S.ManageUsersActions>
         </S.ManageUsers>
     );
